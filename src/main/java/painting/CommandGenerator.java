6326ed2e37f758b591fe0c84df17535a6f5c131b
sen <yjghdsss@163.com>
2021-06-30 17:42:05 +0800

    add lib

--- src/main/java/painting/CommandGenerator.java
+++ src/main/java/painting/CommandGenerator.java
@@ -7,9 +7,6 @@ import java.util.ArrayList;
 public class CommandGenerator {
     int maxWidth, maxHeight;
 
-
-
-
     public static ArrayList<DisplayCommand> generateCommand(LayoutBox box){
         ArrayList<DisplayCommand> commands = new ArrayList<>();
         //todo
